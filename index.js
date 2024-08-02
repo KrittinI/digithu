6d15df741d9a80e622a2e0f99f6ca47c8136307a
KrittinI <krittin.ikk@gmail.com>
2024-08-02 14:18:06 +0700

    done A

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("hello");
+// A
+const max = (arr) => {
+    let number = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < number) continue;
+        number = arr[i]
+    }
+    return number
+}
+
+const secondMax = (arr) => {
+    let number = arr[0]
+    let checkMax = 0
+    const maxNumber = max(arr)
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < number) continue;
+        if (arr[i] === maxNumber && checkMax) {
+            continue
+            checkMax++
+        }
+        number = arr[i]
+    }
+    return number
+}
+
+// const result = max([-1, 4, 30, 2, -4])
+// const result = secondMax([-1, 4, 30, 2, -4])
+// const result = secondMax([3, 4, 5, 6, 7])
+// const result = max([3, 4, 5, 6, 7])
+const result = secondMax([3, 4, 5, 7, 7])
+
+console.log(result);

d34a51f2dfad173d61ae1525f80f61f51355a57f
KrittinI <krittin.ikk@gmail.com>
2024-08-02 14:28:38 +0700

    finish B

--- index.js
+++ index.js
@@ -23,10 +23,32 @@ const secondMax = (arr) => {
     return number
 }
 
-// const result = max([-1, 4, 30, 2, -4])
-// const result = secondMax([-1, 4, 30, 2, -4])
-// const result = secondMax([3, 4, 5, 6, 7])
-// const result = max([3, 4, 5, 6, 7])
-const result = secondMax([3, 4, 5, 7, 7])
+// const resultA = max([-1, 4, 30, 2, -4])
+// const resultA = secondMax([-1, 4, 30, 2, -4])
+// const resultA = secondMax([3, 4, 5, 6, 7])
+// const resultA = max([3, 4, 5, 6, 7])
+const resultA = secondMax([3, 4, 5, 7, 7])
 
-console.log(result);
+
+//  B
+const fn = (arr, number) => {
+    let max = 0
+    if (arr.length < number) {
+        return -1
+    }
+    for (let i = 0; i < arr.length + 1 - number; i++) {
+        let sum = 0
+        for (let j = 0; j < number; j++) {
+            sum += arr[i + j]
+        }
+        if (max < sum) {
+            max = sum
+        }
+    }
+    return max
+}
+
+// const resultB = fn([1, 4, -1, 2, 3], 3)
+const resultB = fn([1, 4, -1, 2, 3], 2)
+
+console.log(resultB);
